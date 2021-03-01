9f6325e3ec8b7c3e61065a4b839df574375494d8
Jason Hodges <Jason.Hodges@resolutefp.com>
2021-03-01 13:44:35 -0600

    initial commit

---
+++ app/WebApp/config.py
@@ -0,0 +1,13 @@
+
+class  Config:
+    SECRET_KEY = '5797b5d9ed82336ac5405da41cff88ef'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///databases/site.db'
+
+
+
+            # Resolute smtp = smtp.cacc.local
+    MAIL_SERVER = 'smtp.cacc.local'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = "jason.hodges@Resolutefp.com"
+    MAIL_PASSWORD = "Passw9rd"
